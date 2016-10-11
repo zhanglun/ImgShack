3a64b55653b08807982f3589026ccace60b79378
cloudZQY <498219667@qq.com>
2016-10-11 17:35:11 +0800

    增加前端获取七牛token的方法

--- src/resource/index.js
+++ src/resource/index.js
@@ -1,4 +1,13 @@
-const a = () => {
-  console.log(1);
-}
-a();
+import Util from './util/util'
+
+
+//要上传的空间
+var bucket = 'Bucket_Name';
+
+//上传到七牛后保存的文件名
+var key = 'my-nodejs-logo.png';
+var param = {};
+param.scope = bucket+":"+key;
+param.callbackUrl = 'http://your.domain.com/callback';
+param.callbackBody = 'filename=$(fname)&filesize=$(fsize)';
+console.log(Util.getToken(param));
