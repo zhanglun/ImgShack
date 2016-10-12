c64ba2786af2eba14a7203ff58b928d1d02c2a91
cloudZQY <498219667@qq.com>
2016-10-12 14:40:28 +0800

    修复upload两次bug，上传成功！

--- src/resource/components/App.vue
+++ src/resource/components/App.vue
@@ -20,25 +20,27 @@
     mounted() {
       console.log('!!!app');
 
-        let bucket = 'blog';
+        let bucket = 'zhanglun';
 //上传到七牛后保存的文件名
         var param = {};
         param.scope = bucket;
-        let uploadToken = createToken(param);
+        let uploadToken = createToken(param, 100000);
 
-        console.log(uploadToken);
         let uploader = createUploader({
             browse_button: 'go',
             drop_element: 'uploader',
             container: 'container',
-            domain: 'http://7xnrrd.com1.z0.glb.clouddn.com',
+            domain: 'http://7i7gl0.com1.z0.glb.clouddn.com',
             token: uploadToken,
         });
         console.log(uploader);
         uploader.bind('PostInit', function(){
-            console.log(arguments);
+            console.log('init!', arguments);
         });
-        uploader.init();
+        uploader.bind('FileUploaded', function(up, file, info){
+            console.log(up, file, info)
+        });
+
     }
   }
 </script>
