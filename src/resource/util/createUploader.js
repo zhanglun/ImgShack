48c5ce268697bc5b86a07426d37a070fd442fb19
zhanglun <zhanglun1410@gmail.com>
2016-10-13 17:21:48 +0800

    上传返回自定义文件名

--- src/resource/util/createUploader.js
+++ src/resource/util/createUploader.js
@@ -1,3 +1,4 @@
+import md5 from 'md5';
 /**
  * 初始化上传组件
  * @param  {Object} opt
@@ -19,13 +20,7 @@ export const createUploader = (opt) => {
     // 'uptoken_url': CONFIG.APIROOT + '/qiniu/token',
     // Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
     uptoken: opt.token,
-    // 若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
-    // unique_names: true,
-    // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
-    // save_key: true,
-    // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
     domain: opt.domain,
-    // bucket 域名，下载资源时用到，**必需**
     container: opt.container || 'container', // 上传区域DOM ID，默认是browser_button的父元素，
     'max_file_size': '100mb', // 最大文件体积限制
     'flash_swf_url': './lib/plupload/Moxie.swf', // 引入flash,相对路径
@@ -69,11 +64,7 @@ export const createUploader = (opt) => {
       'Key': function (up, file) {
         // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
         // 该配置必须要在 unique_names: false , save_key: false 时才生效
-        // var key = "";
-        // do something with key here
-        //   return 'test_prefixer_hahhahah_______' + file.name;
-        // return window.account.username + '/' + file.name;
-        // return opt.setkey(up, file);
+        return md5(new Date() + Math.random() +  file.name) + '.' + file.name.split('.').pop();
       },
     },
   });
