e5742f70a2bb4c738bf7e3fdafc1e6a3c2bfa05a
zhanglun <zhanglun1410@gmail.com>
2016-10-16 17:34:50 +0800

    vuex init

--- src/resource/index.js
+++ src/resource/index.js
@@ -1,12 +1,14 @@
 import './css/base.less';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 
 import UploaderView from './components/Uploader.vue';
 import SettingsView from './components/Settings.vue';
 import HistoryView from './components/History.vue';
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.config.devtools = true;
 Vue.config.debug = true;
@@ -15,7 +17,7 @@ const routes = [
   { path: '/upload', component: UploaderView },
   { path: '/history', component: HistoryView },
   { path: '/settings', component: SettingsView },
-  { path: '*', redirect: '/upload'}
+  { path: '*', redirect: '/upload' }
 ];
 
 
@@ -24,6 +26,23 @@ const router = new VueRouter({
   linkActiveClass: 'nav-item__link--active'
 });
 
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  }
+});
+
+store.commit('increment');
+store.commit('increment');
+
+console.log(store.state.count); // -> 1
+
 const app = new Vue({
-  router
-}).$mount('#app')
+  router,
+  store
+}).$mount('#app');
